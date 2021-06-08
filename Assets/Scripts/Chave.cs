1fa12a3b4c0336e49b3dc7ab426a47d3d8250eb0
HOLLYFNX <62623407+HOLLYFNX@users.noreply.github.com>
2021-06-08 14:55:02 -0300

    NewProject

---
+++ Assets/Scripts/Chave.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Chave : MonoBehaviour
+{
+    public int IDdaChave;
+    public float DistanciaDaChave = 3;
+    public AudioClip somChave;
+    private bool PegouChave;
+    private GameObject Jogador;
+    void Start()
+    {
+        PegouChave = false;
+        Jogador = GameObject.FindWithTag("Player");
+    }
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Jogador.transform.position) < DistanciaDaChave)
+        {
+            if (Input.GetKeyDown("e") && PegouChave == false)
+            {
+                Porta.ListaDeIDs.Add(IDdaChave);
+                PegouChave = true;
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<AudioSource>().PlayOneShot(somChave);
+                Destroy(gameObject, 2.0f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerVida.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerVida : MonoBehaviour
+{
+    public static float VIDA = 100;
+    void Update()
+    {
+        if (VIDA <= 0)
+        {
+            Debug.Log("morreu");
+        }
+    }
+}
