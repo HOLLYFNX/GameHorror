45594c41d6ef73d82167d161f87ca9a11794fb89
HOLLYFNX <62623407+HOLLYFNX@users.noreply.github.com>
2021-06-09 12:57:03 -0300

    aplicando inspect

---
+++ Assets/Scripts/ExaminarObj.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExaminarObj : MonoBehaviour
+{
+       
+    void Update()
+    {
+        Ray ray = Camera.main.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(GetComponent<Collider>().Raycast(ray, out hit, 500f))
+        {
+            print("Sobre o" + gameObject.name);
+        }
+    }
+}
