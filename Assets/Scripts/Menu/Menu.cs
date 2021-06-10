b95f81af42bba937f17c4b452d2c393acfa0dc8e
renanzinhoRZ <renan.alves.teixeira123@gmail.com>
2021-06-10 14:32:30 -0300

    menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public string cena;
+    //public GameObject PanelOpitions;
+    public GameObject PanelCreditos;
+    public GameObject PanelLoad;
+
+    public void StartGame()
+    {
+        PanelLoad.SetActive(true);
+        SceneManager.LoadScene(cena);
+
+    }
+
+    public void quietGame()
+    {
+
+        //editor unity
+        UnityEditor.EditorApplication.isPlaying = false;
+        //jogo compilado 
+        //Application.Quit();
+
+    }
+   
+
+    public void ShowCredit()
+    {
+
+        PanelCreditos.SetActive(true);
+
+    }
+
+    public void BackToMenu()
+    {
+
+        PanelCreditos.SetActive(false);
+
+    }
+}
